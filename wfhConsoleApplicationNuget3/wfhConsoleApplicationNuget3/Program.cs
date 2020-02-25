4018e4f0150dcb84ec7f13af7b32442036e68518
unknown <1054534334@qq.com>
2020-02-25 23:16:04 +0800

    first commit

---
+++ wfhConsoleApplicationNuget3/wfhConsoleApplicationNuget3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfhConsoleApplicationNuget3
+{
+    public class ProgramText
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(add(2, 3));
+        }
+        public static int add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
+
